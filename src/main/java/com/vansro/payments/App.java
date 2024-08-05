3eda20aa6fdba0ba1de41b270e830cb6d1a209b3
EC2 Default User <ec2-user@ip-172-31-1-163.ap-south-1.compute.internal>
2024-08-05 13:50:37 +0000

    1st Commit

---
+++ src/main/java/com/vansro/payments/App.java
@@ -0,0 +1,16 @@
+package com.vansro.payments;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Welcome to Vansro" );
+        System.out.println( "Student Login Page" );
+        System.out.println( "Enter User Id:" );
+        System.out.println( "Enter Password:" );
+    }
+}
